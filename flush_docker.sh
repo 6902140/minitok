f7b01a4c12996d16e71205f8254038e7f367c677
3356142450@qq.com <3356142450@qq.com>
2023-08-11 18:28:42 +0800

    修改flush脚本

--- flush_docker.sh
+++ flush_docker.sh
@@ -4,7 +4,7 @@
 sudo docker ps -a -q | xargs sudo docker rm
 
 # 删除名为 "minitok_tiktoklite" 的镜像
-sudo docker rmi minitok_tiktoklite
+sudo docker rmi minitok_minitok
 
 # 执行 docker-compose up
 sudo docker-compose up
